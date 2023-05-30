2b308b8b9c85573eb58d25f3120f4a3e158fb9a0
Marco Ramirez <marco.ramirez.a@hotmail.com>
2023-05-30 04:25:56 -0500

    The Explanation for this Modle Sucks

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (driver) => driver.slice(0,2) 
+
+const  returnLastTwoDrivers = (driver) => driver.slice (2,4)
+
+const  selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+function createFareMultiplier(n){
+    return function () {
+    return n*n
+   }
+}
+const fareDoubler = (fare) => fare*2
+const  fareTripler = (fare) => fare * 3
+
+
+const selectDifferentDrivers = (selectingDrivers, array2) => array2(selectingDrivers)
